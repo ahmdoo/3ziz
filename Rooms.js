5edb98977940f06ec33ec8524c8ae76de1d50b52
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2023-02-05 01:56:32 +0300

    Create Rooms.js

---
+++ Rooms.js
@@ -0,0 +1,32 @@
+$(`<style>.nosel.d-flex.room.r185733aacfax813d4dd6x-t59vbn.bord {border-inline: 4px solid #fff !important;}
+
+
+
+
+
+.nosel.d-flex.room.r185733aacfax813d4dd6x-t59vbn .fitimg.u-pic.borderg{border-radius: 100px!important;border: 1px solid #737373;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.0,.0,.0,.0) alternate infinite;}
+
+
+
+.nosel.d-flex.room.r185733aacfax813d4dd6x-t59vbn .label-primary {background-color: transparent !important;etter-spacing: 1px;-webkit-text-fill-color: #fff;padding: 0px !important;border-radius: 2px !important;}
+
+
+
+ 
+.nosel.d-flex.room.r185733aacfax813d4dd6x-t59vbn .corner.fa.fa-user.label.uc.fa-microphone.label-danger {background-image: url(https://g.top4top.io/p_2576zxw9j1.jpg);letter-spacing: 1px;-webkit-text-fill-color: ##ddd;padding: 0px !important;border-radius: 50px !important;background-color: #737373;}
+
+}</style>`).insertBefore('body');
+var rid='185733aacfax813d4dd6x-t59vbn';
+
+
+$(`<style>#mic.aym3 .mic {
+        border: 2px solid!important;
+        border-image: linear-gradient(var(--angle), #737373 0%, #fff 29%, #737373 67%, #fff 100%) 1!important;
+        animation: 5s rotate linear infinite!important;
+        --angle: 0deg!important;
+        background-color:#fff!important;
+        border-radius: 50px !important;
+        margin: 2px!important;margin-top: 1px!important;box-shadow: inset 0 0px 0px rgb(0 0 0 / 8%), 0 0px 1px #ffffff!important;
+        height: 48px!important;}
+        
+        @keyframes rotate{
