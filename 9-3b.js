00dcb3621494ca153614399514297f7b83a1c424
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2023-02-07 03:41:03 +0300

    Update 9-3b.js

--- 9-3b.js
+++ 9-3b.js
@@ -819,21 +819,21 @@ var Aziz=setInterval(function(){
         $("#users .uzr.stl1 span.uhash, #users .uzr.stl1 img.co").hide(); 
 
 
-        $("#upro").before('<div id="3ziz" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">'); 
+        $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">'); 
         
         
         
         $("div#users .uzr:contains('- قِسْطَاسْ #فِي_المَنْفَى') .My_vedio1").click(function(){
-        $("#3ziz").show();
+        $("#movon1").show();
         
-        $('#3ziz').addClass('stl1');
-        $("#3ziz").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
+        $('#movon1').addClass('stl1');
+        $("#movon1").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
         
         
-        $("#3ziz").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');
+        $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');
           
 		  
-        $("#3ziz").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/svA75h-GX68" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/svA75h-GX68" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
         
        $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
@@ -841,21 +841,3 @@ var Aziz=setInterval(function(){
 });
     }
 }, 10000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
