edead7375a1b0938a776b28ecf0e3369893dad5b
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2023-02-11 23:44:07 +0300

    Update and rename A7meed.js to A7med.js

--- A7med.js
+++ A7med.js
@@ -8,7 +8,7 @@ $("#users .uzr.ahm span.uhash, #users .uzr.ahm img.co,#users .uzr.ahm .ustat,.fl
 
 $("style").last().append(`
 #users .uzr.ahm .fitimg.u-pic {
-  border-radius: 0px 8px 8px 0px!important;
+  border-radius: 0px 6px 6px 0px!important;
 }
 #users .uzr.ahm {
     background-color:#EDEDED;
