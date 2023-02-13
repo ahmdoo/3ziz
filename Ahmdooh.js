dc80f75d157652494d2de2032c07ccd11a4ed2f4
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2023-02-13 04:19:33 +0300

    Update and rename A7med.js to Ahmdooh.js

---
+++ Ahmdooh.js
@@ -0,0 +1,17 @@
+$.getScript("https://raw.githack.com/ahmdoo/3ziz/main/A7med.js");
+
+var lail=setInterval(function(){
+    if(myid!=null){
+        clearInterval(lail);
+$("#users .uzr:contains('- ʟ')").addClass('lil');             
+$("#users .uzr.lil span.uhash, #users .uzr.lil img.co,#users .uzr.lil .ustat,.fl.mini.u-co").hide();
+    }
+}, 10000);
+
+$("style").last().append(`
+#users .uzr.lil .u-topic {
+  margin-left: 5px;
+}
+
+`);
+
